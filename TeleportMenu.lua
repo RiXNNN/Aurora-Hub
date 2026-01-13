bc10111ade173ef06ea87f52f089c83ec6017590
RiXNNN <147681476+RiXNNN@users.noreply.github.com>
2026-01-13 17:06:21 +0200

    Update TeleportMenu.lua

--- TeleportMenu.lua
+++ TeleportMenu.lua
@@ -11,7 +11,7 @@ end
 
 TeleportModule.Data = {
     Places = {
-        ["SafeZone"] = {-405, 8, -3945},
+        ["SafeZone"] = {9267,-3751,16824},
         ["Hayakawa Village"] = {474, 755, -1994},
         ["Okuiya Village"] = {-3204, 704, -1165},
         ["Kamakura Village"] = {-2148, 1161, -1675},
@@ -20,8 +20,7 @@ TeleportModule.Data = {
         ["Final Selection"] = {-5195, 792, -3045},
         ["Ubuyashiki Mansion"] = {-1968, 874, -6500},
         ["Crystal Boss Gate"] = {-3062, 845, 1162},
-        ["Infinity Castle Entry"] = {1992, 1574, -7314},
-        ["Frosty Forest"] = {1601, 950, -7015}
+        ["Infinity Castle Entry"] = {-9936,6898,-4614}
     },
     Perfect_Crystals = {
         ["Perfect Crystal 1"] = {24, 732, -232},
@@ -42,20 +41,20 @@ TeleportModule.Data = {
         ["Trinket Merchant (Okuiya)"] = {-4199, 834, 141}
     },
     Trainers_NPCs = {
-        ["Flower (Kanoe)"] = {-1282, 871, -6435},
-        ["Wind (Grimm)"] = {-3296, 703, -1256},
+        ["Flower Breath (Kanoe)"] = {-1320,871,-6240},
+        ["Wind Breath (Grimm)"] = {-3296, 703, -1256},
         ["Sanemi Raid NPC"] = {-3296, 703, -1256},
-        ["Sun (Tanjiro)"] = {390, 816, -424},
-        ["Moon (Kokushibo)"] = {1826, 1116, -5956},
-        ["Flame (Rengoku)"] = {1503, 1236, -356},
-        ["Water (Urokodaki)"] = {-915, 845, -984},
-        ["Thunder (Jigoro)"] = {-725, 697, 558},
-        ["Insect (Shinobu)"] = {-1641, 908, -6493},
-        ["Sound (Uzui)"] = {-1268, 871, -6435},
-        ["Mist (Muichiro)"] = {3242, 778, -4048},
-        ["Love (Mitsuri)"] = {1179, 1077, -1107},
-        ["Beast (Inosuke)"] = {-3106, 779, -6595},
-        ["Serpent (Obanai)"] = {-7936, 2430, -824},
+        ["Sun Breath (Tanjiro)"] = {390, 816, -424},
+        ["Moon Breath (Kokushibo)"] = {1826, 1116, -5956},
+        ["Flame Breath (Rengoku)"] = {1503, 1236, -356},
+        ["Water Breath (Urokodaki)"] = {-915, 845, -984},
+        ["Thunder Breath (Jigoro)"] = {-725, 697, 558},
+        ["Insect Breath (Shinobu)"] = {-1641, 908, -6493},
+        ["Sound Breath (Uzui)"] = {-1268, 871, -6435},
+        ["Mist Breath (Muichiro)"] = {3242, 778, -4048},
+        ["Love Breath (Mitsuri)"] = {1179, 1077, -1107},
+        ["Beast Breath (Inosuke)"] = {-3106, 779, -6595},
+        ["Serpent Breath (Obanai)"] = {-7936, 2430, -824},
         ["Muzan NPC"] = {1992, 1574, -7321},
         ["Kaigaku NPC"] = {-2828, 779, -3258}
     },
